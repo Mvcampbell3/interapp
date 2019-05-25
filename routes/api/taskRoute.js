86205ac184f02d742d0bdbb3e674eed819a550b2
Mvcampbell3 <mvcampbell3@gmail.com>
2019-05-25 15:00:54 -0400

    added in newtask post route

--- routes/api/taskRoute.js
+++ routes/api/taskRoute.js
@@ -7,4 +7,18 @@ router.get("/all", (req,res) => {
     .catch(err => res.json(err));
 })
 
+router.post("/newtask", (req,res) => {
+  if (req.user) {
+    const newTask = new Task(req.body);
+    newTask.userID = req.user._id;
+
+    newTask.save()
+      // Need to push result._id into User Tasks array 
+      .then(result =>  res.json(result))
+      .catch(err => res.json(err))
+  } else {
+    res.json({user:false})
+  }
+})
+
 module.exports = router;
