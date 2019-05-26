eab3a6b9f9a2380ca15a7769133e84fb93f107bd
Mvcampbell3 <mvcampbell3@gmail.com>
2019-05-26 16:47:08 -0400

    more task routing

--- routes/api/taskRoute.js
+++ routes/api/taskRoute.js
@@ -2,12 +2,15 @@ const router = require("express").Router();
 const Task = require("../../models/Task");
 const User = require("../../models/User");
 
+// Get all of the tasks, regardless of user, this will be removed or gated
 router.get("/all", (req, res) => {
   Task.find()
     .then(result => res.json(result))
     .catch(err => res.json(err));
 })
 
+// New Task route, adds in userID through req.user
+// Pushes created taskID to user tasks
 router.post("/newtask", (req, res) => {
 
   let saved, updated;
@@ -36,13 +39,35 @@ router.post("/newtask", (req, res) => {
   }
 })
 
+// Get all of the tasks associated with that user
 router.get("/user", (req, res) => {
   if (req.user) {
     Task.find({ userID: req.user._id })
       .then(result => res.json(result))
       .catch(err => res.json(err))
   } else {
-    res.json({user:false})
+    res.json({ user: false })
+  }
+})
+
+router.delete("/deltask/:id", (req, res) => {
+  if (req.user) {
+    Task.findById(req.params.id)
+      .then(task => {
+        if (task.userID === req.user._id) {
+          task.remove()
+            .then(result => res.json({
+              message: "successfully deleted task",
+              taskDeleted: result
+            }))
+            .catch(err => res.json(err))
+        } else {
+          res.json("can not delete")
+        }
+      })
+      .catch(err => res.json(err));
+  } else {
+    res.json({ user: false })
   }
 })
 
--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -2,13 +2,15 @@ const router = require("express").Router();
 const passport = require("../../config/passport");
 const User = require("../../models/User");
 
+// Get all of the users, will be gated or deleted
 router.get("/all", (req,res) => {
   User.find()
     .then(result => res.json(result))
     .catch(err => res.json(err))
 })
 
-router.get("/test", (req,res) => {
+// Returns user populated with their tasks information
+router.get("/withtasks", (req,res) => {
   if (req.user) {
     // res.json({user:true})
     User.findById(req.user._id)
@@ -21,6 +23,7 @@ router.get("/test", (req,res) => {
   }
 })
 
+// Creates new user, model handles the bcrypting of password
 router.post("/signup", (req, res) => {
   const newUser = new User(req.body);
 
@@ -32,10 +35,12 @@ router.post("/signup", (req, res) => {
     .catch(err => res.json(err));
 });
 
+// Passport local strategy login authentication, returns boolean
 router.post("/login", passport.authenticate("local"), (req, res) => {
   res.json({ user: true });
 });
 
+// Logs user out from passport
 router.get("/logout", function(req, res) {
   req.logout();
   res.json({ loggedOut: true });
